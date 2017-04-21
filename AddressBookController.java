08391c802115a6f8a4326d19793e96e88695e2af
snehalakkar <snehalakkar@gmail.com>
2017-04-21 19:46:14 +0530

    address book with serialization

--- AddressBookController.java
+++ AddressBookController.java
@@ -1,5 +1,12 @@
 package com.bridgelabz.controller;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -23,9 +30,7 @@ import com.bridgelabz.tester.CheckUniqueness;
 public class AddressBookController<linkedList> implements AddressBook {
 	Scanner scanner = new Scanner(System.in);
 	Person person = new Person();
-	//HashSet<Long> hashSet = new HashSet<>();
-	//Service service = new Service();
-	CheckUniqueness ch=new CheckUniqueness();
+	CheckUniqueness ch = new CheckUniqueness();
 	LinkedList<Person> linkedList = new LinkedList<Person>();
 	HashMap<String, LinkedList> hashMap = new HashMap<String, LinkedList>();
 	String addressBookName;
@@ -35,6 +40,7 @@ public class AddressBookController<linkedList> implements AddressBook {
 		System.out.println("\n give name to address book ");
 		String addressBookName = scanner.next();
 		System.out.println("Addres book name is :" + addressBookName);
+		hashMap = deseriazable();
 		if (hashMap.containsKey(addressBookName)) {
 			System.out.println("\n address book is already exist with same name ,do u want to create another?(yes/no)");
 			String ans = scanner.next();
@@ -43,15 +49,19 @@ public class AddressBookController<linkedList> implements AddressBook {
 			}
 		} else {
 			hashMap.put(addressBookName, new LinkedList<Person>());
+			serializable();
 		}
 	}
 
 	/*
-	 * (non-Javadoc)Adding person details to the address book
+	 * Adding person details to the address book
 	 * 
 	 * @see com.bridgelabz.service.AddressBook#addPerson(java.lang.String)
 	 */
 	public void addPerson(String addressBookName1) {
+
+		hashMap = deseriazable();
+		System.out.println(hashMap);
 		if (hashMap.containsKey(addressBookName1)) {
 			person = new Person();
 			// linkedList = new LinkedList<Person>();
@@ -69,63 +79,64 @@ public class AddressBookController<linkedList> implements AddressBook {
 			person.setState(state);
 			System.out.println("\nEnter zip code :");
 			int zip = scanner.nextInt();
-			int length1=Integer.toString(zip).length();//to check the length of int 
-			if(length1==6){
+			int length1 = Integer.toString(zip).length();// to check the length
+															// of int
+			if (length1 == 6) {
 				person.setZip(zip);
-			}
-			else{
+			} else {
 				System.out.println("Invalid lengh of zip code it must be 6 digits only");
 				System.out.println("\nEnter zip code :");
 				int zip1 = scanner.nextInt();
-				int length=Integer.toString(zip1).length();
-				if(length==6){
+				int length = Integer.toString(zip1).length();
+				if (length == 6) {
 					person.setZip(zip);
 				}
 			}
 			System.out.println("\nEnter 10 digit phone number :");
 			long phone = scanner.nextLong();
-			int length2=Long.toString(phone).length();
-			if(length2==10){
-				if(ch.chechUniqueness(phone)){
-                person.setPhone(phone);
-			}
-				else{
-					System.out.println("This phone number is already registered  ");       
-				
-				
+			int length2 = Long.toString(phone).length();
+			if (length2 == 10) {
+				if (ch.chechUniqueness(phone)) {
+					person.setPhone(phone);
+				} else {
+					System.out.println("This phone number is already registered  ");
+
+				}
+			} else {
+				System.out.println("Invalid length of phone number it must be 10 digits only");
+				System.out.println("\nEnter 10 digit phone number :");
+				long phone1 = scanner.nextLong();
+				int length3 = Long.toString(phone1).length();
+				if (length3 == 10) {
+					// person.setPhone(phone1);
+					if (ch.chechUniqueness(phone))
+						person.setPhone(phone1);
 				}
 			}
-		else{
-			System.out.println("Invalid length of phone number it must be 10 digits only");
-			System.out.println("\nEnter 10 digit phone number :");
-			long phone1 = scanner.nextLong();
-			int length3=Long.toString(phone1).length();
-			if(length3==10){
-				//person.setPhone(phone1);
-				if(ch.chechUniqueness(phone))
-					person.setPhone(phone1);
+			// person.display();
+
+			if (hashMap.get(addressBookName1) == null) {
+				hashMap.put(addressBookName1, new LinkedList<Person>());
 			}
-		}
-		//person.display();
+			hashMap.get(addressBookName1).add(person);
 
-		if (hashMap.get(addressBookName1) == null) {
-			hashMap.put(addressBookName1, new LinkedList<Person>());
-		}
-		hashMap.get(addressBookName1).add(person);
+			System.out.println("\n" + hashMap);
+			// person.display();
+			linkedList = hashMap.get(addressBookName1);
+			for (int i = 0; i < linkedList.size(); i++) {
+				Person p1 = linkedList.get(i);
+				System.out.println("person name is :" + p1.getFirstName());
+				p1.display();
+			}
 
-		//System.out.println("\n" + hashMap);
-
-		linkedList = hashMap.get(addressBookName1);
-		for (int i = 0; i < linkedList.size(); i++) {
-			Person p1 = linkedList.get(i);
-			System.out.println("person name is :"+p1.getFirstName());
-			p1.display();
+			serializable();
 		}
-	}
+
 		else {
 			System.out.println("\n Address book is not in record,u first create address book ");
 			createAddressBook();
 		}
+
 	}
 
 	/*
@@ -139,7 +150,7 @@ public class AddressBookController<linkedList> implements AddressBook {
 		String deletename = scanner.next();
 		System.out.println("\n Enter the firstname of user which record you want to delete ");
 		String first = scanner.next();
-		@SuppressWarnings("unchecked")
+		hashMap = deseriazable();
 		LinkedList<Person> linkedList = hashMap.get(deletename);
 
 		if (linkedList != null) {
@@ -160,7 +171,7 @@ public class AddressBookController<linkedList> implements AddressBook {
 			System.out.println(per);
 		}
 		System.out.println("\n" + hashMap);
-
+		serializable();
 	}
 
 	/*
@@ -174,71 +185,80 @@ public class AddressBookController<linkedList> implements AddressBook {
 		String updatename = scanner.next();
 		System.out.println("\nEnter first name :");
 		String first = scanner.next();
+		hashMap = deseriazable();
 		boolean flag = hashMap.containsKey(updatename);
+		Person p1 = null;
 		if (flag) {
-			LinkedList<Person> linkedList = hashMap.get(updatename);
-			for (int i = 0; i < linkedList.size(); i++) {
-				Person p1 = linkedList.get(i);
+			LinkedList<Person> linkedList1 = hashMap.get(updatename);
+			for (int i = 0; i < linkedList1.size(); i++) {
+				p1 = linkedList1.get(i);
 				if (p1.getFirstName().equalsIgnoreCase(first)) {
 					System.out.println("\nU can update now   ");
 					System.out.println("\nEnter first name :");
 					String firstName = scanner.next();
-					person.setFirstName(firstName);
+					p1.setFirstName(firstName);
 					System.out.println("\nEnter last name :");
 					String lastName = scanner.next();
-					person.setLastName(lastName);
+					p1.setLastName(lastName);
 					System.out.println("\nEnter address :");
 					String address = scanner.next();
-					person.setAddress(address);
+					p1.setAddress(address);
 					System.out.println("\nEnter state name :");
 					String state = scanner.next();
-					person.setState(state);
+					p1.setState(state);
 					System.out.println("\nEnter zip code :");
 					int zip = scanner.nextInt();
-					int length1 = Integer.toString(zip).length();// to check length of int
+					int length1 = Integer.toString(zip).length();// to check
+																	// length of
+																	// int
 					if (length1 == 6) {
-						person.setZip(zip);
+						p1.setZip(zip);
 					} else {
 						System.out.println("invalid lengh of zip code it must be 6 digits only");
 						System.out.println("\nEnter zip code :");
 						int zip1 = scanner.nextInt();
 						int length = Integer.toString(zip1).length();
 						if (length == 6) {
-							person.setZip(zip);
+							p1.setZip(zip);
 						}
 					}
 					System.out.println("\nEnter phone number :");
+
 					long phone = scanner.nextLong();
 					int length2 = Long.toString(phone).length();
 					if (length2 == 10) {
-						if(ch.chechUniqueness(phone))
-						person.setPhone(phone);
+						if (ch.chechUniqueness(phone))
+							p1.setPhone(phone);
 					} else {
 						System.out.println("Invalid length of phone number it must be 10 digits only");
 						System.out.println("\nEnter 10 digit phone number :");
 						long phone1 = scanner.nextLong();
 						int length3 = Long.toString(phone1).length();
 						if (length3 == 10) {
-							if(ch.chechUniqueness(phone1))
-							person.setPhone(phone1);
+							if (ch.chechUniqueness(phone1))
+								p1.setPhone(phone1);
 						}
 					}
+
 					System.out.println("\nUpdate successfull ....");
-					// person.display();
 				}
 			}
+			System.out.println(hashMap);
+			serializable();
 		}
+
 		linkedList = hashMap.get(updatename);
 		for (int i = 0; i < linkedList.size(); i++) {
-			Person p1 = linkedList.get(i);
-			System.out.println("person name is :" + p1.getFirstName());
-			p1.display();
+			Person p11 = linkedList.get(i);
+			System.out.println("person name is :" + p11.getFirstName());
+			p11.display();
 		}
-		// System.out.println("\n" + hashMap);
+
+		System.out.println(hashMap);
 	}
 
 	/*
-	 * (non-Javadoc)sorting an person from linkedlist based on their firstname
+	 * sorting an person from linkedlist based on their firstname
 	 * 
 	 * @see com.bridgelabz.service.AddressBook#sortByName()
 	 */
@@ -247,6 +267,7 @@ public class AddressBookController<linkedList> implements AddressBook {
 		System.out.println("\n Sort by name ");
 		System.out.println("\n Enter the address book which you want to sort ");
 		String name = scanner.next();
+		hashMap = deseriazable();
 		if (hashMap.containsKey(name)) {
 			LinkedList<Person> linkedList = hashMap.get(name);
 			Collections.sort(linkedList, Person.personName);
@@ -254,10 +275,11 @@ public class AddressBookController<linkedList> implements AddressBook {
 				System.out.println(per);
 			}
 		}
+		serializable();
 	}
 
 	/*
-	 * (non-Javadoc)sorting an person from linkedlist based on their zipcode
+	 * sorting an person from linkedlist based on their zipcode
 	 * 
 	 * @see com.bridgelabz.service.AddressBook#sortByZip()
 	 */
@@ -266,6 +288,7 @@ public class AddressBookController<linkedList> implements AddressBook {
 		System.out.println("\n Sort by zip ");
 		System.out.println("\n Enter the address book which you want to sort ");
 		String name = scanner.next();
+		hashMap = deseriazable();
 		if (hashMap.containsKey(name)) {
 			LinkedList<Person> linkedList = hashMap.get(name);
 			Collections.sort(linkedList, Person.zipcode);
@@ -273,11 +296,11 @@ public class AddressBookController<linkedList> implements AddressBook {
 				System.out.println(per);
 			}
 		}
+		serializable();
 	}
 
 	/*
-	 * (non-Javadoc)searching person from any address book based on unique phone
-	 * number
+	 * searching person from any address book based on unique phone number
 	 * 
 	 * @see com.bridgelabz.service.AddressBook#searchPerson()
 	 */
@@ -285,6 +308,7 @@ public class AddressBookController<linkedList> implements AddressBook {
 	public void searchPerson() {
 		System.out.println("\n Enter the phone number of person whose record you want to search  ");
 		long searchnumber = scanner.nextLong();
+		hashMap = deseriazable();
 		Set<String> keys = hashMap.keySet();
 		boolean flag = false;
 		for (String key : keys) {
@@ -306,7 +330,35 @@ public class AddressBookController<linkedList> implements AddressBook {
 		if (!flag) {
 			System.out.println("u entered invalid phone number ");
 		}
+		serializable();
+	}
 
+	public void serializable() {
+		try {
+			FileOutputStream fileOutputStream = new FileOutputStream(
+					"/home/bridgeit/JavaPrograms/addressbooknew/serializedfile.ser");
+			BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream);
+			ObjectOutputStream objectOutputStream = new ObjectOutputStream(bufferedOutputStream);
+			objectOutputStream.writeObject(hashMap);
+			objectOutputStream.flush();
+			objectOutputStream.close();
+		} catch (IOException ex) {
+			System.out.println(ex);
+		}
+	}
+
+	public HashMap deseriazable() {
+		try {
+			FileInputStream fileInputStream = new FileInputStream(
+					"/home/bridgeit/JavaPrograms/addressbooknew/serializedfile.ser");
+			BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
+			ObjectInputStream objectInputStream = new ObjectInputStream(bufferedInputStream);
+			hashMap = (HashMap<String, LinkedList>) objectInputStream.readObject();
+			objectInputStream.close();
+		} catch (IOException | ClassNotFoundException ex) {
+			System.out.println(ex);
+		}
+		return hashMap;
 	}
 
 }
