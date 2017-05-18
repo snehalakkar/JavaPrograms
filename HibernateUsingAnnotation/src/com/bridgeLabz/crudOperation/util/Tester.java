479b0b7d43c26294cc1073490bad5bfbb27c4182
snehalakkar <snehalakkar@gmail.com>
2017-05-18 11:51:47 +0530

    crud using annoation

--- HibernateUsingAnnotation/src/com/bridgeLabz/crudOperation/util/Tester.java
+++ HibernateUsingAnnotation/src/com/bridgeLabz/crudOperation/util/Tester.java
@@ -10,13 +10,13 @@ public static void main(String[] args) {
 	Student s1=new Student();
 //no need to set id bcoz it is primary key and already set using annotation id and generated
 	
-	s1.setBranch("java");
-	s1.setName("snel");
+	s1.setBranch("sql");
+	s1.setName("soni");
 	
 	StudentDao sdao=new StudentDao();
 	sdao.save(s1);
 	
-	sdao.update(6,"yash");
+	sdao.update(6,"yash1");
 
 	sdao.delete(4);
 	
